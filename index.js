49bd9bf5c079ea620493e8a8c0b574fc67f5a159
yamashitayohei <yamanobu.yamashita@gmail.com>
2020-04-19 02:37:29 +0900

    third commit

--- index.js
+++ index.js
@@ -10,17 +10,35 @@ function init() {
     camera.position.z = 1;
  
     scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xf0f0f0 );
  
     geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
     material = new THREE.MeshNormalMaterial();
  
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
+
+    var loader = new THREE.FontLoader();
+    loader.load('fonts/helvetiker_regular.typeface.json', function ( font ) {
+        var textGeometry = new THREE.TextGeometry("Hello Three.js!", {
+            font: font,
+            size: 20,
+            height: 5,
+            curveSegments: 12
+        });
+        var materials = [
+            new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff, overdraw: 0.5 } ),
+            new THREE.MeshBasicMaterial( { color: 0x000000, overdraw: 0.5 } )
+        ];
+        var textMesh = new THREE.Mesh(textGeometry, materials);
+        scene.add(textMesh);
+    } );
  
     renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth-20, window.innerHeight-25);
     document.body.appendChild( renderer.domElement );
  
+    window.addEventListener( 'resize', onWindowResize, false );
 }
  
 function animate() {
@@ -32,4 +50,10 @@ function animate() {
  
     renderer.render( scene, camera );
  
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth-20, window.innerHeight-25 );
 }
